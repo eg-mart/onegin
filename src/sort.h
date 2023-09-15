01bb567e1681d9ccda584fd6bff16bd7b709490b
eg-mart <marteg71@gmail.com>
2023-09-15 21:51:57 +0300

    add include quards

--- src/sort.h
+++ src/sort.h
@@ -1,5 +1,10 @@
+#ifndef SORT
+#define SORT
+
 void qsort(void *arr, size_t size, size_t left, size_t right,
 		   int (*comp)(const void *, size_t, size_t));
 int compare_str(const void *str, size_t a, size_t b);
 int compare_str_reverse(const void *data, size_t a, size_t b);
 int compare_int(const void *data, size_t a, size_t b);
+
+#endif
