c10405682b0b069828408e45beafe53838d41634
eg-mart <marteg71@gmail.com>
2023-09-18 18:54:10 +0300

    refactor sort comparators

--- src/sort.cpp
+++ src/sort.cpp
@@ -9,38 +9,38 @@ size_t partition(void *arr, size_t size, size_t left, size_t right,
 				 int (*comp)(const void *, size_t, size_t));
 void swap(void *arr, size_t left, size_t right, size_t size);
 
-int compare_str(const void *data, size_t a, size_t b)
+int compare_str(const void *data, size_t first, size_t second)
 {
 	const struct Line *lines = (const struct Line *) data;
 
 	size_t i = 0;
-	for (; lines[a].txt[i] != '\0' && lines[b].txt[i] != '\0'; i++) {
-		if (lines[a].txt[i] != lines[b].txt[i])
+	for (; lines[first].txt[i] != '\0' && lines[second].txt[i] != '\0'; i++) {
+		if (lines[first].txt[i] != lines[second].txt[i])
 			break;
 	}
 	
-	return lines[a].txt[i] - lines[b].txt[i];
+	return lines[first].txt[i] - lines[second].txt[i];
 }
 
-int compare_str_reverse(const void *data, size_t f_line, size_t s_line)
+int compare_str_reverse(const void *data, size_t first, size_t second)
 {
 	const struct Line *lines = (const struct Line *) data;
 
-	size_t f_char_idx = lines[f_line].len - 1;
-	size_t s_char_idx = lines[s_line].len - 1;
+	size_t i = lines[first].len - 1;
+	size_t j = lines[second].len - 1;
 
-	for (; f_char_idx > 0 && s_char_idx > 0; f_char_idx--, s_char_idx--)
-		if (lines[f_line].txt[f_char_idx] != lines[s_line].txt[s_char_idx])
-			return lines[f_line].txt[f_char_idx] - lines[s_line].txt[s_char_idx];
+	for (; i > 0 && j > 0; i--, j--)
+		if (lines[first].txt[i] != lines[second].txt[j])
+			return lines[first].txt[i] - lines[second].txt[j];
 
-	return (int)(f_char_idx - s_char_idx);
+	return (int)(i - j);
 }
 
-int compare_int(const void *data, size_t a, size_t b)
+int compare_int(const void *data, size_t first, size_t second)
 {
 	const int *arr = (const int *) data;
-	if (arr[a] < arr[b]) return -1;
-	if (arr[a] > arr[b]) return 1;
+	if (arr[first] < arr[second]) return -1;
+	if (arr[first] > arr[second]) return 1;
 	return 0;
 }
 
